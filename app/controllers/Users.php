12262f6d8d34ef8cc5fe5c56e238d1f87e644388
Danujaya  Liyanage <2021is054@stu.ucsc.cmb.ac.lk>
2023-09-29 18:09:33 +0530

    finalize authentication

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -189,4 +189,21 @@ class Users extends Controller
         $_SESSION['user_mobile_no'] = $user->mobile_no;
         redirect('pages/index');
     }
+    public function logout()
+    {
+        unset($_SESSION['user_id']);
+        unset($_SESSION['user_name']);
+        unset($_SESSION['user_mobile_no']);
+        session_destroy();
+        redirect('users/login');
+    }
+
+    public function isLoggedIn(){
+        if(isset($_SESSION['user_id'])){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 }
