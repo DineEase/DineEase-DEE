303dad0599a81f9bc4f73bc06cd5a4756e6049bc
Danujaya  Liyanage <2021is054@stu.ucsc.cmb.ac.lk>
2023-09-30 14:24:33 +0530

    add views for all the users

---
+++ app/views/receptionist/index.php
@@ -0,0 +1,14 @@
+<?php require APPROOT . '/views/inc/header-general.php'; ?>
+<div class="container">
+    <div class="navbar-template">
+        <?php require APPROOT . '/views/inc/topbar.php'; ?>
+    </div>
+    <div class="sidebar-template">
+        <?php require APPROOT . '/views/inc/sidebar-receptionist.php'; ?>
+    </div>
+    <div class="body-template">
+        <?php
+        ?>
+    </div>
+</div>
+<?php require APPROOT . '/views/inc/footer-general.php'; ?>
