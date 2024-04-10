b70ed74e2d9e2d9941318cef043baa45dc584ae8
Danujaya  Liyanage <2021is054@stu.ucsc.cmb.ac.lk>
2024-04-10 12:04:34 +0530

    Update jQuery version to 3.7.1  (Local)

--- app/views/inventoryManager/inventory.php
+++ app/views/inventoryManager/inventory.php
@@ -291,7 +291,7 @@
             </div>
         </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="<?php echo URLROOT; ?>/js/jquery-3.7.1.js"></script>
     <script src="<?php echo URLROOT; ?>/js/customer.js"></script>
     <script src="<?php echo URLROOT; ?>/js/inventorymanager.js"></script>
     
