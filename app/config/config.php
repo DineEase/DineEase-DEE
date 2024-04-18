8532d725f46ce0d4963b264db6e95758ec7360f7
Danujaya  Liyanage <2021is054@stu.ucsc.cmb.ac.lk>
2024-04-18 07:32:28 +0530

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -7,7 +7,7 @@ define('DB_USER', 'root');
 // define('DB_USER', 'appoavpmum');
 define('DB_PASS', '');
 // define('DB_PASS', 'mPLiL6EEs$PGxfht');
-define('DB_NAME', 'de_db_v4');
+define('DB_NAME', 'de_db_v2');
 //app root
 define('APPROOT', dirname(dirname(__FILE__)));
 //URL Root
