5c14b295205f3530b9fd245e1446db4231809fd8
Danujaya  Liyanage <2021is054@stu.ucsc.cmb.ac.lk>
2024-04-28 07:32:01 +0530

    Fix formatting issues in multiple views

--- app/views/chef/profile.php
+++ app/views/chef/profile.php
@@ -5,99 +5,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/common.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/customer-styles.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/profile.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/toastr.css ">
     <link rel="icon" type="image/x-icon" href="<?php echo URLROOT ?>/public/img/login/favicon.ico">
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet">
     <title><?php echo SITENAME; ?></title>
-    <style>
-        .dashboard-head {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            width: 97.6%;
-            padding: 20px;
-            height: 60px;
-        }
-
-        .receptionist-dashboard-container {
-            display: flex;
-            flex-direction: column;
-            padding: 5px;
-        }
-
-        .rdh-item {
-            display: flex;
-            flex-direction: row;
-            justify-content: center;
-            align-items: center;
-            width: 30%;
-            height: 100%;
-        }
-
-        .rdh-date-picekr {
-            display: flex;
-            flex-direction: row;
-            justify-content: center;
-            align-items: center;
-            gap: 10px;
-        }
-
-        .rdh-date-picekr span {
-            cursor: pointer;
-        }
-
-        .rdh-date-picekr input {
-            padding: 5px;
-            border: none;
-            border-radius: 5px;
-            outline: none;
-            font-size: x-large;
-        }
-
-        .dashboard-content {
-            display: flex;
-            flex-direction: column;
-            height: 100vh;
-            padding: 25px;
-        }
-
-        .receptionist-dashboard-container table {
-            width: 100%;
-            height: 100%;
-            border-collapse: collapse;
-            border: var(--brandgreen) solid 4px;
-            border-radius: 10px;
-        }
-
-        .receptionist-dashboard-container table tr {
-            height: 50px;
-        }
-
-        .receptionist-dashboard-container table tr td {
-            border: var(--brandgreen) solid 1px;
-            text-align: center;
-        }
-
-        .receptionist-dashboard-container table tr td:nth-child(1) {
-            width: 10% !important;
-        }
-
-
-        .view-slot-button {
-            background-color: var(--brandgreen);
-            color: white;
-            padding: 10px 20px;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-            transition: background-color 0.3s;
-        }
-
-        .view-slot-button:hover {
-            background-color: var(--brandgreen-dark);
-        }
-    </style>
 </head>
+<style>
+    /* Modern look for input fields */
+</style>
 
 <body>
     <div class="container">
@@ -113,7 +32,9 @@
                     </div>
                     <div class="navbar-content">
                         <div class="profile-details">
-
+                            <!-- <span class="material-symbols-outlined topbar-shoping-cart" value="0">
+                                shopping_cart_off
+                            </span> -->
                             <span class="material-symbols-outlined material-symbols-outlined-topbar  topbar-notifications">notifications </span>
                             Hello, &nbsp; <?php echo ucfirst($_SESSION['role']) ?> <span class="user-name"> &nbsp; | &nbsp; <?php echo  $_SESSION['user_name'] ?></span>
                             <img src="<?php echo URLROOT ?>/img/profilePhotos/<?php echo $_SESSION['profile_picture'] ?>" alt="profile-photo" class="profile" />
@@ -141,10 +62,10 @@
                                     </button>
                                 </a>
                             </li>
-                  
+
                             <li class="item">
                                 <a href="<?php echo URLROOT ?>/chefs/order" class="nav_link" onclick="changeContent('order')">
-                                    <button class="button-sidebar-menu">
+                                    <button class="button-sidebar-menu ">
                                         <span class="navlink_icon">
                                             <span class="material-symbols-outlined ">
                                                 list_alt
@@ -190,11 +111,166 @@
                 </div>
             </nav>
         </div>
-        <div class="body-template" id="content">
-            
+        <div class="body-template">
+            <?php flash('password_error'); ?>
+            <?php flash('password_success'); ?>
+            <div id="content">
+                <div id="overlay-profile" class="overlay-profile"></div>
+
+                <div class="profile-container">
+                    <!-- //TODO: Add the form to change user details -->
+                    <div id="change-password-div" class="change-password-div">
+                        <form action="<?php echo URLROOT; ?>/profiles/changePassword" class="change-password" method="post">
+                            <h2>Change User Name & Password</h2>
+                            <label for="old-psw">Old Password</label>
+                            <input type="password" id="old-psw" name="old-psw" required>
+                            <label for="new-psw">New Password</label>
+                            <input type="password" id="new-psw" name="new-psw" required>
+                            <label for="confirm-psw">Confirm New Password</label>
+                            <input type="password" id="confirm-psw" name="confirm-psw" required>
+                            <input type="submit" value="Submit">
+                        </form>
+                    </div>
+                    <div class="user-header">
+                        <h2>User Profile</h2>
+                        <img class="heroimage" src="<?php echo URLROOT ?>/img/profilePhotos/<?php echo $_SESSION['profile_picture'] ?>" alt="Avatar">
+                    </div>
+                    <div class="buttons">
+                        <button id="change-user-password" class="change-btn"><i class="fa-solid fa-lock"></i> Change Password</button>
+                        <button class="update" id="update-dp"><i class="fa-solid fa-square-pen"></i> Update Picture</button>
+                    </div>
+                    <!-- //TODO: Add the form to change user Details -->
+
+                    <!-- //TODO  Delete the old images when an new image is uploaded -->
+                    <div id="overlay-profile" class="overlay-profile"></div>
+                    <div class="profilecard">
+                        <div class="card-body">
+                            <form action="<?php echo URLROOT; ?>/profiles/updateUserDetails" method="post">
+                                <table>
+                                    <tr>
+                                        <td>
+                                            <p class="profdetails">User Name</p>
+                                        </td>
+                                        <td>:</td>
+                                        <td><input type="text" name="user_name" value="<?php echo htmlspecialchars($_SESSION['user_name']); ?>" readonly></td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <p class="profdetails">Email Address</p>
+                                        </td>
+                                        <td>:</td>
+                                        <td><input type="email" name="email" value="<?php echo htmlspecialchars($_SESSION['email']); ?>" readonly></td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <p class="profdetails">Contact Number</p>
+                                        </td>
+                                        <td>:</td>
+                                        <td><input type="text" name="mobile_no" value="<?php echo htmlspecialchars($_SESSION['mobile_no']); ?>" readonly></td>
+                                    </tr>
+                                    <tr>
+                                        <button type="button" id="change-user-details" class="change-btn">Change Details</button>
+                                        <button type="button" id="cancel-user-details" class="change-btn" style="display:none;">Cancel</button>
+                                        <input type="submit" id="update-user-details" class="change-btn" value="Update Details" style="display:none;">
+
+                                    </tr>
+                                </table>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="upload-container" id="upload-container">
+                        <h2>Upload New Profile Picture:</h2>
+                        <hr>
+                        <form action="<?php echo URLROOT ?>/profiles/uploadUserImage" method="post" enctype="multipart/form-data">
+                            <label for="file-upload" class="file-input">
+                                <input type="file" class="uploadb-file-input" id="file-upload" name="photo" accept=".jpg, .jpeg, .png" required>
+                                <span id="file-name" class="uploadb-file-name"></span>
+                            </label>
+                            <button type="submit" id="upload-dp-btn" class="upload-btn button-disabled">Upload Photo</button>
+                        </form>
+                    </div>
+
+                    <div class="loyalty-container">
+                        <h2>Loyalty Points</h2>
+                        <p>Your current loyalty points: <span class="loyalty-points">250</span></p>
+                    </div>
+
+                </div>
+            </div>
         </div>
     </div>
     <script src="<?php echo URLROOT; ?>/js/jquery-3.7.1.js"></script>
+    <!-- <script src="<?php echo URLROOT; ?>/js/customer.js"></script> -->
+    <script src="<?php echo URLROOT; ?>/js/user-profile.js"></script>
+    <script src="<?php echo URLROOT; ?>/js/toastr.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            // Handling success messages
+            <?php if (isset($_SESSION['ppsuccess_message'])) : ?>
+                toastr.success('<?php echo $_SESSION['ppsuccess_message']; ?>');
+                <?php unset($_SESSION['ppsuccess_message']); ?>
+            <?php endif; ?>
+
+            <?php if (isset($_SESSION['pwsuccess_message'])) : ?>
+                toastr.success('<?php echo $_SESSION['pwsuccess_message']; ?>');
+                <?php unset($_SESSION['pwsuccess_message']); ?>
+            <?php endif; ?>
+
+            // Handling general error notifications
+            <?php if (isset($_SESSION['error_message'])) : ?>
+                toastr.error('<?php echo $_SESSION['error_message']; ?>');
+                <?php unset($_SESSION['error_message']); ?>
+            <?php endif; ?>
+
+            // Handling specific error notifications for password change
+            <?php if (isset($_SESSION['password_error'])) : ?>
+                toastr.error('<?php echo $_SESSION['password_error']; ?>');
+                <?php unset($_SESSION['password_error']); ?>
+            <?php endif; ?>
+
+            // Handling multiple errors stored in an array
+            <?php if (isset($_SESSION['errors']) && is_array($_SESSION['errors'])) : ?>
+                <?php foreach ($_SESSION['errors'] as $error) : ?>
+                    toastr.error('<?php echo $error; ?>');
+                <?php endforeach; ?>
+                <?php unset($_SESSION['errors']); ?>
+            <?php endif; ?>
+        });
+
+        document.getElementById('change-user-details').addEventListener('click', function() {
+            var mobileInput = document.querySelector('input[name="mobile_no"]');
+            var emailInput = document.querySelector('input[name="email"]');
+            var userName = document.querySelector('input[name="user_name"]');
+            userName.readOnly = false;
+            emailInput.readOnly = false;
+            mobileInput.readOnly = false;
+
+            mobileInput.addEventListener('input', function(e) {
+                var nonNumericRemoved = this.value.replace(/\D/g, '');
+                if (nonNumericRemoved.length > 10) {
+                    nonNumericRemoved = nonNumericRemoved.substr(0, 10);
+                }
+                this.value = nonNumericRemoved;
+            });
+
+            document.getElementById('update-user-details').style.display = 'inline';
+            document.getElementById('cancel-user-details').style.display = 'inline';
+            document.getElementById('change-user-details').style.display = 'none';
+        });
+        document.getElementById('cancel-user-details').addEventListener('click', function() {
+            var mobileInput = document.querySelector('input[name="mobile_no"]');
+            var emailInput = document.querySelector('input[name="email"]');
+            var userName = document.querySelector('input[name="user_name"]');
+            userName.readOnly = true;
+            emailInput.readOnly = true;
+            mobileInput.readOnly = true;
+
+            document.getElementById('update-user-details').style.display = 'none';
+            document.getElementById('cancel-user-details').style.display = 'none';
+            document.getElementById('change-user-details').style.display = 'inline';
+        });
+    </script>
+
 </body>
 
 </html>
